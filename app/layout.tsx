a706a3f550ab8ffeb5833d3ccac03c45baf523c8
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-08-05 13:26:13 +0530

    add create room functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ReactNode } from "react";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -33,7 +34,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
