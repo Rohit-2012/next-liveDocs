120e716f3fdf40fb9d45db324e7fc4b6221f27c7
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-08-05 12:10:12 +0530

    Add Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,41 @@
-import { Inter as FontSans } from "next/font/google"
-import './globals.css'
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
 
-import { cn } from "@/lib/utils"
-import { ReactNode } from "react"
-import { Metadata } from "next"
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Next LiveDocs',
-  description: 'A next-level collaborative editor',
-}
+  title: "Next LiveDocs",
+  description: "A next-level collaborative editor",
+};
 
-export default function RootLayout({ children }: {children: ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
