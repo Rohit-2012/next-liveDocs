73b68c96186fcd1a87486bf2de775a15ba3bcb2d
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-08-06 11:51:33 +0530

    create collaborative room

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -5,22 +5,26 @@ import Loader from "./Loader";
 import Header from "./Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Editor } from "./editor/Editor";
+import ActiveCollaborators from "./ActiveCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="document-title">Share</p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -25,7 +25,7 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ['room:write'],
     });
 
       revalidatePath("/");
@@ -35,3 +35,20 @@ export const createDocument = async ({
     console.log(`Something went wrong while creating the room: ${error} `);
   }
 };
+
+export const getDocument = async ({ roomId, userId }: { roomId: string, userId: string }) => {
+  try {
+    const room = await liveblocks.getRoom(roomId)
+
+  // const hasAccess = Object.keys(room.usersAccesses).includes(userId)
+
+  // if (!hasAccess) {
+  //   throw new Error('You do not have access to this document')
+  // }
+
+  return parseStringify(room)
+  } catch (error) {
+    console.log(`Error occurred while getting a room: ${error}`)
+  }
+  
+}
