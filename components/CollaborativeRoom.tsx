2cd14ecc8f3107de491e7b60ef08a498e9c99cba
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-08-09 12:00:01 +0530

    add live floating comments feature

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,18 +11,13 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 
-interface CollaborativeRoomProps {
-  roomId: string;
-  roomMetadata: {
-    title: string;
-  };
-}
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
+  users,
+  currentUserType
 }: CollaborativeRoomProps) => {
-  const currentUserType = "editor";
 
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
@@ -127,7 +122,7 @@ const CollaborativeRoom = ({
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
